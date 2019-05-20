a18556448108829c0f5bae4260ddd196ee516444
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-20 20:34:48 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var people = [
+  [1, 'Dimitri', 'Hacktiv8'],
+  [2, 'Dhani', 'Hacktiv8'],
+  [3, 'Icha', 'Hacktiv8']
+];
+
+// your code here
+// ID: 1 Name: Dimitri Company: Hacktiv8
+// ID: 2 Name: Dhani Company: Hacktiv8
+// ID: 3 Name: Icha Company: Hacktiv8
