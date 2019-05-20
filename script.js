d0c46728102454ed2e762449adf13a8774764052
Rusydy <rusydymuhiddin@gmail.com>
2019-05-20 21:25:54 +0700

    done

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var people = [
-  [1, 'tony', 'Hacktiv8'],
+  [1, 'Dimitri', 'Hacktiv8'],
   [2, 'Dhani', 'Hacktiv8'],
   [3, 'Icha', 'Hacktiv8']
 ];
+
+for (var i = 0; i < people.length; i++) {
+  console.log("ID: " + people [i][0] + " Name: " + people [i][1] + " Company: " + people [i][2])
+}
 
 // your code here
 // ID: 1 Name: Dimitri Company: Hacktiv8
